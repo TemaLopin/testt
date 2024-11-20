38aee5f88c3716e7f59710ace77a50004c2510c0
Tema Lopin <a.lopin@auchan.ru>
2024-11-21 02:12:09 +0300

    mobile

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css'
 import { MainLayout } from './layouts'
 
 import { PC_Version } from './widgets/pc'
+import { FeetBack, Footer, GameHack, GivePhone, HowToPlay, Main, Merch, NotFromRussia } from './widgets'
+import { WhereBuy } from './shared/ui/where-buy'
 
 const tg = window.Telegram.WebApp
 
@@ -15,16 +17,16 @@ function App() {
 
   return (
     <MainLayout>
-      {/* <Main /> */}
-      <PC_Version />
-      {/*   <HowToPlay />
+      <Main />
+      {/* <PC_Version /> */}
+      <HowToPlay />
       <GivePhone />
       <NotFromRussia />
       <GameHack />
       <Merch />
       <FeetBack />
       <WhereBuy />
-      <Footer /> */}
+      <Footer />
     </MainLayout>
   )
 }
